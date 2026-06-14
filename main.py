85b9e477b106707ff8e38257f29086d6728e1300
Muneeb-ur-rehman543 <muneeb430430@gmail.com>
2026-06-14 07:17:50 +0000

    Added main.py code

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from student import *
+from book import *
+from transaction import *
 
+while True:
+    print("\nLibrary Management System")
+    print("1. Student Module")
+    print("2. Book Module")
+    print("3. Transaction Module")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        print("Student Module Active")
+    elif choice == "2":
+        print("Book Module Active")
+    elif choice == "3":
+        print("Transaction Module Active")
+    elif choice == "4":
+        break
+    else:
+        print("Invalid Choice")
